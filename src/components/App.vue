301f632c430adf6ff1891c59993c621169b6b6d2
Tim Eulitz <tim.eulitz@wikimedia.de>
2019-04-29 14:55:51 +0200

    Rename wmde19 skin to "Laika" skin

---
+++ src/components/App.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<header>
+			<Header :page-identifier="pageIdentifier" :assets-path="assetsPath"></Header>
+		</header>
+		<main id="app" class="section">
+			<div class="container">
+				<Content>
+					<slot></slot>
+					<template v-slot:sidebar>
+						<slot name="sidebar"></slot>
+					</template>
+				</Content>
+			</div>
+		</main>
+		<footer>
+			<Footer :assets-path=assetsPath></Footer>
+		</footer>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Header from './layout/Header.vue';
+import Content from './layout/Content.vue';
+import Footer from './layout/Footer.vue';
+
+export default Vue.extend( {
+	name: 'app',
+	components: {
+		Header,
+		Content,
+		Footer,
+	},
+	props: [ 'assetsPath', 'pageIdentifier' ],
+} );
+</script>
+
+<style lang="scss">
+	@import "../scss/variables";
+	@import "../scss/classes";
+
+	// Import Bulma's core
+	@import "~bulma/sass/utilities/_all";
+
+	// Overriding Bulma variables
+	@import "../scss/overrides";
+
+	// Import Bulma and Buefy styles
+	@import "~bulma";
+	@import "~buefy/src/scss/buefy";
+	@import "~bulma-helpers/sass/helpers/spacing/margin-padding";
+</style>
---
+++ src/components/layout/Content.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="container has-margin-top-108 has-margin-bottom-54">
+		<div class="columns">
+			<main class="column is-two-thirds has-background-bright">
+				<slot></slot>
+			</main>
+			<slot name="sidebar"></slot>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Content',
+};
+</script>
