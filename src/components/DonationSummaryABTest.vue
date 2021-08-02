182ad1c3467b8a9b798e0c4c1acf21978c4c5c7d
Abban Dunne <abban.dunne@wikimedia.de>
2021-08-02 10:13:40 +0200

    Update alt payment summary to same as inline
    This changes only the alt version of the donation
    confirmation. It now uses the same language items
    as the inline summary.
    
    Once the test ends and if the alt version is successful
    we should move the duplicated language selector into a
    wrapper component.
    
    Ticket: https://phabricator.wikimedia.org/T287354

--- src/components/DonationSummaryABTest.vue
+++ src/components/DonationSummaryABTest.vue
@@ -15,7 +15,7 @@
 				:address-type="addressType"
 				:address="address"
 				:countries="countries"
-				:language-item="'donation_confirmation_topbox_summary_alt'"
+				:language-item="inlineSummaryLanguageItem"
 			/>
 		</feature-toggle>
 	</div>
@@ -24,6 +24,8 @@
 <script lang="js">
 import Vue from 'vue';
 import DonationSummary from '@/components/shared/DonationSummary';
+import { NS_ADDRESS } from '@/store/namespaces';
+import { AddressTypeModel } from '@/view_models/AddressTypeModel';
 
 export default Vue.extend( {
 	name: 'DonationSummaryABTest',
@@ -36,5 +38,20 @@ export default Vue.extend( {
 	components: {
 		DonationSummary,
 	},
+	computed: {
+		inlineSummaryLanguageItem() {
+			switch ( this.$store.state[ NS_ADDRESS ].addressType ) {
+				case AddressTypeModel.ANON:
+				case AddressTypeModel.UNSET:
+					return 'donation_confirmation_inline_summary_anonymous';
+				case AddressTypeModel.EMAIL:
+					return 'donation_confirmation_inline_summary_email';
+				case AddressTypeModel.COMPANY:
+				case AddressTypeModel.PERSON:
+				default:
+					return 'donation_confirmation_inline_summary_address';
+			}
+		},
+	}
 } );
 </script>
