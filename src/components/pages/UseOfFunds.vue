d1b03d96d187f9b92ba7970d20c5547034e7f65e
Gabriel Birke <gabriel.birke@posteo.net>
2021-11-12 16:21:12 +0100

    Revert "Hide link to wmde annual plan"
    This reverts commit 85f08a1c5c32dbc0dbf08d8e972236ba46dc9ffa.
    
    Reverting because the annual plan is offical.
    
    See https://phabricator.wikimedia.org/T295467

--- src/components/pages/UseOfFunds.vue
+++ src/components/pages/UseOfFunds.vue
@@ -17,7 +17,7 @@
 					{{ content.detailedReports.international.linkName }}
 				</a>
 			</div>
-			<div class="use_of_funds__column--info" style="display: none;">
+			<div class="use_of_funds__column--info">
 				<span>{{ content.detailedReports.germany.intro }}</span>
 				<a :href="content.detailedReports.germany.linkUrl" target="_blank">
 					{{ content.detailedReports.germany.linkName }}
