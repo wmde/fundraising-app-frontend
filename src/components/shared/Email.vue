bc3d3508b681f8f9ac92b0da5346f5b8fbb4b5c7
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-01-19 14:09:20 +0100

    Add mail provider suggestion
    - checks levensthein distance to one
    of the most used mail providers
    - suggests it to the user below the input field
    
    https://phabricator.wikimedia.org/T296282

--- src/components/shared/Email.vue
+++ src/components/shared/Email.vue
@@ -10,18 +10,38 @@
 				@blur="$emit('field-changed', 'email')">
 			</b-input>
 		</b-field>
+		<span v-if="suggestedProvider" class="help has-text-dark-lighter"> Meinten Sie '{{ suggestedProvider }}'?</span>
 		<span v-if="showError" class="help is-danger">{{ $t( 'donation_form_email_error' ) }}</span>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import { distance, closest } from 'fastest-levenshtein';
+import { AddressFormData } from '@/view_models/Address';
 
 export default Vue.extend( {
 	name: 'Email',
 	props: {
-		formData: Object as () => FormData,
+		formData: Object as () => AddressFormData,
 		showError: Boolean,
+		commonMailProviders: Array,
+	},
+	computed: {
+		suggestedProvider: function () {
+			const mailUserInput = this.formData.email.value;
+			if ( mailUserInput.indexOf( '@' ) === -1 ) {
+				return '';
+			}
+			const mailHost = mailUserInput.substring( mailUserInput.lastIndexOf( '@' ) + 1 );
+			const closestFit = closest( mailHost,
+				[ 'gmail.com', 'yahoo.com', 't-online.de', 'abracadabza.ac', 'gmx.net', 'ab.c', 'web.de' ] );
+			const calculatedDistance = distance( mailHost, closestFit );
+			if ( calculatedDistance > 0 && calculatedDistance <= 2 ) {
+				return closestFit;
+			}
+			return '';
+		},
 	},
 } );
 </script>
