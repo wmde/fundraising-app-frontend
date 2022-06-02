d4e691209ac11087c4e5e1a1d7075f45f1b0dc80
Abban Dunne <abban.dunne@wikimedia.de>
2022-06-02 13:14:30 +0200

    Store referrer on donation form
    This stores the referrer of the donor so
    we know where they've come from when they
    lose their tracking parameters by navigating
    around the site before donating.
    
    Ticket: https://phabricator.wikimedia.org/T306686

--- src/util/CampaignParameters.ts
+++ src/util/CampaignParameters.ts
@@ -1,8 +1,10 @@
 import { CampaignValues } from '@/view_models/CampaignValues';
-import 'core-js/features/url-search-params';
+import Cookies from 'js-cookie';
 
 export const campaignKey = 'piwik_campaign';
 export const keywordKey = 'piwik_kwd';
+export const cookieKey = 'referrer';
+export const defaultKeyword = 'general';
 
 export default class CampaignParameters {
 	private params: URLSearchParams;
@@ -12,9 +14,34 @@ export default class CampaignParameters {
 	}
 
 	public getCampaignValues(): CampaignValues {
+		const campaign = this.params.get( campaignKey );
+		const keyword = this.params.get( keywordKey );
+
+		if ( campaign && keyword ) {
+			Cookies.set( cookieKey, this.parseReferrer( campaign, keyword ) );
+			return { campaign, keyword };
+		}
+
+		const referrer = Cookies.get( cookieKey );
+
+		if ( referrer ) {
+			return {
+				campaign: referrer,
+				keyword: defaultKeyword,
+			};
+		}
+
 		return {
-			campaign: this.params.get( campaignKey ) ?? '',
-			keyword: this.params.get( keywordKey ) ?? '',
+			campaign: '',
+			keyword: '',
 		};
+	}
+
+	private parseReferrer( campaign: string, keyword: string ): string {
+		if ( campaign.includes( 'org' ) || keyword.includes( 'org' ) ) {
+			return 'WMF';
+		}
+
+		return 'WMDE';
 	}
 }
