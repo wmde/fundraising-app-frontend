9e34864824e13000292063028cc3d83fa9e7e11b
Gabriel Birke <gabriel.birke@wikimedia.de>
2022-08-11 19:40:58 +0200

    Fix membership fee validation
    With the introduction of the payment type, the server-side validation
    can now only run when the type is filled too. This commit changes the
    validation behavior of the store as follows:
    
    1. Check if all membership fee values have been filled before sending
       data to the server for validation.
    2. Reuse the existing validation action of the store in the actions that
       trigger the server-side validation.
    3. Validation mutations for interval and type can either receive the new
       validity value or check for non-empty values themselves (when called
       without parameter).
    
    Fix type in ValidationResponse - it had the wrong definition but since
    it was a "faux type" (assumed type of JSON data sent by the server,
    without runtime checking) the code still worked

--- src/store/membership_fee/actions.ts
+++ src/store/membership_fee/actions.ts
@@ -1,21 +1,8 @@
 import { ActionContext } from 'vuex';
 
-import {
-	IntervalData,
-	SetFeePayload,
-	MembershipFee,
-	InitialMembershipFeeValues,
-} from '@/view_models/MembershipFee';
+import { InitialMembershipFeeValues, IntervalData, MembershipFee, SetFeePayload } from '@/view_models/MembershipFee';
 
-import {
-	initializeMembershipFee,
-	markEmptyFeeAsInvalid,
-	markEmptyValuesAsInvalid,
-	setFee,
-	setInterval,
-	validateFee,
-	setType,
-} from '@/store/membership_fee/actionTypes';
+import { initializeMembershipFee, markEmptyFeeAsInvalid, markEmptyValuesAsInvalid, setFee, setInterval, setType, validateFee } from '@/store/membership_fee/actionTypes';
 import {
 	MARK_EMPTY_FEE_INVALID,
 	MARK_EMPTY_FIELDS_INVALID,
@@ -29,13 +16,25 @@ import { ValidationResponse } from '@/store/ValidationResponse';
 import { Validity } from '@/view_models/Validity';
 import { Helper } from '@/store/util';
 import { validateFeeDataRemotely } from '@/store/axios';
-import { TypeData } from '@/view_models/Payment';
 import { SET_TYPE, SET_TYPE_VALIDITY } from '@/store/payment/mutationTypes';
+
+/**
+ * Map source values from WMDE\Fundraising\MembershipContext\Domain\MembershipPaymentValidator to validation mutation functions
+ * We'll use this map to process the result from the server-side validation
+ */
+const fieldToValidationMutation = new Map( [
+	[ 'fee', SET_FEE_VALIDITY ],
+	[ 'interval', SET_INTERVAL_VALIDITY ],
+	[ 'type', SET_TYPE_VALIDITY ],
+] );
+
+const validateFeeOnClientSide = ( fee: string ): Validity => Helper.isNonNumeric( fee ) ? Validity.INVALID : Validity.VALID;
 
 export const actions = {
 	[ initializeMembershipFee ]( context: ActionContext<MembershipFee, any>, initialData: InitialMembershipFeeValues ) {
 		if ( initialData.fee ) {
 			context.commit( SET_FEE, initialData.fee );
+			context.commit( SET_FEE_VALIDITY, validateFeeOnClientSide( initialData.fee ) );
 		}
 
 		if ( initialData.interval ) {
@@ -48,17 +47,11 @@ export const actions = {
 			context.commit( SET_TYPE_VALIDITY );
 		}
 
-		if ( initialData.fee || initialData.interval ) {
-			context.commit( SET_IS_VALIDATING, true );
-			validateFeeDataRemotely(
-				context,
-				initialData.validateFeeUrl,
-				context.state.values.fee,
-				context.state.values.interval,
-				context.state.values.type,
-			).then( ( validationResult: ValidationResponse ) => {
-				context.commit( SET_FEE_VALIDITY, validationResult.status === 'ERR' ? Validity.INVALID : Validity.VALID );
-				context.commit( SET_IS_VALIDATING, false );
+		// Trigger server-side validation to restore server-side validation state
+		if ( context.getters.allPaymentValuesAreSet ) {
+			return context.dispatch( validateFee, {
+				feeValue: initialData.fee,
+				validateFeeUrl: initialData.validateFeeUrl,
 			} );
 		}
 	},
@@ -77,42 +70,41 @@ export const actions = {
 			context.state.values.interval,
 			context.state.values.type,
 		).then( ( validationResult: ValidationResponse ) => {
-			context.commit( SET_FEE_VALIDITY, validationResult.status === 'ERR' ? Validity.INVALID : Validity.VALID );
+			fieldToValidationMutation.forEach( ( mutationFunction, errorSource ) => {
+				context.commit( mutationFunction, Helper.validationSucceeded( validationResult, errorSource ) );
+			} );
 			context.commit( SET_IS_VALIDATING, false );
 		} );
 	},
 	[ setFee ]( context: ActionContext<MembershipFee, any>, payload: SetFeePayload ): Promise<void> {
 		context.commit( SET_FEE, payload.feeValue );
-		if ( Helper.isNonNumeric( payload.feeValue ) ) {
-			context.commit( SET_FEE_VALIDITY, Validity.INVALID );
-			return Promise.resolve();
+		context.commit( SET_FEE_VALIDITY, validateFeeOnClientSide( payload.feeValue ) );
+
+		// Trigger server-side validation on full completion
+		if ( context.getters.allPaymentValuesAreSet ) {
+			return context.dispatch( validateFee, payload );
 		}
-		if ( Helper.isNonNumeric( context.state.values.interval ) ) {
-			context.commit( SET_INTERVAL_VALIDITY );
-			return Promise.resolve();
-		}
-		return context.dispatch( validateFee, payload );
+		return Promise.resolve();
 	},
 	[ setInterval ]( context: ActionContext<MembershipFee, any>, payload: IntervalData ): Promise<void> {
 		context.commit( SET_INTERVAL, payload.selectedInterval );
+
+		// Trigger client-side validation - store will inspect set value
 		context.commit( SET_INTERVAL_VALIDITY );
-		if ( Helper.isNonNumeric( context.state.values.fee ) ) {
-			return Promise.resolve();
+
+		// Trigger server-side validation on full completion
+		if ( context.getters.allPaymentValuesAreSet ) {
+			return context.dispatch( validateFee, {
+				feeValue: context.state.values.fee,
+				validateFeeUrl: payload.validateFeeUrl,
+			} );
 		}
-		context.commit( SET_IS_VALIDATING, true );
-		return validateFeeDataRemotely(
-			context,
-			payload.validateFeeUrl,
-			context.state.values.fee,
-			context.state.values.interval,
-			context.state.values.type,
-		).then( ( validationResult: ValidationResponse ) => {
-			context.commit( SET_FEE_VALIDITY, validationResult.status === 'ERR' ? Validity.INVALID : Validity.VALID );
-			context.commit( SET_IS_VALIDATING, false );
-		} );
+		return Promise.resolve();
 	},
-	[ setType ]( context: ActionContext<MembershipFee, any>, payload: TypeData ): void {
-		context.commit( SET_TYPE, payload );
+	[ setType ]( context: ActionContext<MembershipFee, any>, typeName: string ): void {
+		context.commit( SET_TYPE, typeName );
+		// Trigger client-side validation - store will inspect set value
 		context.commit( SET_TYPE_VALIDITY );
+		// We don't trigger a server-side validation here, because we don't have a validation URL
 	},
 };
