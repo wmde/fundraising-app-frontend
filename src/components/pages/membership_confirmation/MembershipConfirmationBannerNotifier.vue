eb95be41f61b93a55064e6a9a1e7964ffb3aabad
Abban Dunne <abban.dunne@wikimedia.de>
2022-09-15 09:44:22 +0200

    Add new banner membership confirmation notifiers
    Add new membership confirmation img tags that
    ping Wikipedia to tell it to not show banners.
    
    Ticket: https://phabricator.wikimedia.org/T316775

---
+++ src/components/pages/membership_confirmation/MembershipConfirmationBannerNotifier.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<img src="https://de.wikipedia.org/wiki/Special:HideBanners?category=fr-thankyou&duration=15552000&reason=membership"
+			alt=""
+			width="0"
+			height="0"
+		/>
+		<img src="https://de.m.wikipedia.org/wiki/Special:HideBanners?category=fr-thankyou&duration=15552000&reason=membership"
+			alt=""
+			width="0"
+			height="0"
+		/>
+		<img src="https://en.wikipedia.org/wiki/Special:HideBanners?category=fr-thankyou&duration=15552000&reason=membership"
+			alt=""
+			width="0"
+			height="0"
+		/>
+		<img src="https://en.m.wikipedia.org/wiki/Special:HideBanners?category=fr-thankyou&duration=15552000&reason=membership"
+			alt=""
+			width="0"
+			height="0"
+		/>
+	</div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default Vue.extend( {
+	name: 'MembershipConfirmationBannerNotifier',
+} );
+</script>
