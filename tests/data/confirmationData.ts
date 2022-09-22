832d74ffb6f6db8f8faa7d8c6d550481b34c2bfc
Abban Dunne <abban.dunne@wikimedia.de>
2022-09-22 08:59:18 +0200

    Style standard donation success
    Ticket: https://phabricator.wikimedia.org/T316628

--- tests/data/confirmationData.ts
+++ tests/data/confirmationData.ts
@@ -1,53 +1,76 @@
 export const testBankTransferCode = 'XW-XLK-M3F-Z';
 
-export const bankTransferConfirmationData = {
-	addressType: 'person',
-	address: {
-		city: 'Berlin',
-		countryCode: 'DE',
-		email: 'test@wikimedia.de',
-		firstName: 'Tester',
-		fullName: 'Prof. Dr. Tester McTest',
-		lastName: 'McTest',
-		postalCode: '10963',
-		salutation: 'Herr',
-		streetAddress: 'Tempelhofer Ufer 23-24',
-	},
+function createConfirmationData( overrides: any ) {
+	return Object.assign( {}, {
+		addressType: 'person',
+		address: {
+			city: 'Berlin',
+			countryCode: 'DE',
+			email: 'test@wikimedia.de',
+			firstName: 'Tester',
+			fullName: 'Prof. Dr. Tester McTest',
+			lastName: 'McTest',
+			postalCode: '10963',
+			salutation: 'Herr',
+			streetAddress: 'Tempelhofer Ufer 23-24',
+		},
+		donation: {
+			accessToken: 'a839bc8045aba4c8b600bc0477dbbf10',
+			amount: 12.35,
+			bankTransferCode: '',
+			id: 1,
+			interval: 0,
+			optsIntoDonationReceipt: true,
+			optsIntoNewsletter: false,
+			paymentType: '',
+			updateToken: 'd387cebd6cc05efbd117545492cb0e99',
+		},
+		countries: [
+			{
+				'countryCode': 'DE',
+				'countryFullName': 'Deutschland',
+				'isFrequentCountry': true,
+				'postCodeValidation': '^[0-9]{5}$',
+			},
+			{
+				'countryCode': 'AT',
+				'countryFullName': 'Österreich',
+				'isFrequentCountry': true,
+				'postCodeValidation': '^[0-9]{4}$',
+			},
+		],
+		salutations: [
+			{
+				'label': 'Herr',
+				'value': 'Herr',
+				'display': 'Herr',
+			},
+			{
+				'label': 'Frau',
+				'value': 'Frau',
+				'display': 'Frau',
+			},
+		],
+	}, overrides );
+}
+
+export const bankTransferConfirmationData = createConfirmationData( {
 	donation: {
-		accessToken: 'a839bc8045aba4c8b600bc0477dbbf10',
-		amount: 12.35,
-		bankTransferCode: testBankTransferCode,
-		id: 1,
-		interval: 0,
-		optsIntoDonationReceipt: true,
-		optsIntoNewsletter: false,
 		paymentType: 'UEB',
-		updateToken: 'd387cebd6cc05efbd117545492cb0e99',
+		bankTransferCode: testBankTransferCode,
 	},
-	countries: [
-		{
-			'countryCode': 'DE',
-			'countryFullName': 'Deutschland',
-			'isFrequentCountry': true,
-			'postCodeValidation': '^[0-9]{5}$',
-		},
-		{
-			'countryCode': 'AT',
-			'countryFullName': 'Österreich',
-			'isFrequentCountry': true,
-			'postCodeValidation': '^[0-9]{4}$',
-		},
-	],
-	salutations: [
-		{
-			'label': 'Herr',
-			'value': 'Herr',
-			'display': 'Herr',
-		},
-		{
-			'label': 'Frau',
-			'value': 'Frau',
-			'display': 'Frau',
-		},
-	],
-};
+} );
+
+export const payPalConfirmationData = createConfirmationData( {
+	donation: {
+		paymentType: 'PPL',
+		bankTransferCode: testBankTransferCode,
+	},
+} );
+
+export const directDebitConfirmationData = createConfirmationData( {
+	donation: {
+		paymentType: 'BEZ',
+		bankTransferCode: testBankTransferCode,
+	},
+} );
---
+++ tests/unit/components/pages/donation_confirmation/SuccessMessage.spec.ts
@@ -0,0 +1,150 @@
+import { createLocalVue, mount } from '@vue/test-utils';
+import { createStore } from '@/store/donation_store';
+import SuccessMessage from '@/components/pages/donation_confirmation/SuccessMessage.vue';
+import { directDebitConfirmationData, payPalConfirmationData } from '../../../../data/confirmationData';
+
+const localVue = createLocalVue();
+
+describe( 'SuccessMessage', () => {
+	it( 'renders messages', () => {
+		const wrapper = mount( SuccessMessage, {
+			localVue,
+			propsData: {
+				donation: payPalConfirmationData.donation,
+				commentLinkIsDisabled: true,
+			},
+			store: createStore(),
+			mocks: {
+				$t: ( key: string ) => key,
+				$n: () => {},
+			},
+		} );
+
+		expect( wrapper.text() ).toContain( 'donation_confirmation_topbox_payment_title_alt' );
+		expect( wrapper.text() ).toContain( 'donation_confirmation_inline_summary' );
+	} );
+
+	it( 'does not render newsletter confirmation message when opted out', () => {
+		const wrapper = mount( SuccessMessage, {
+			localVue,
+			propsData: {
+				donation: payPalConfirmationData.donation,
+				commentLinkIsDisabled: true,
+			},
+			store: createStore(),
+			mocks: {
+				$t: ( key: string ) => key,
+				$n: () => {},
+			},
+		} );
+
+		expect( wrapper.text() ).not.toContain( 'donation_confirmation_newsletter_confirmation' );
+	} );
+
+	it( 'renders newsletter confirmation message when opted in', () => {
+		let donation = payPalConfirmationData.donation;
+		donation.optsIntoNewsletter = true;
+
+		const wrapper = mount( SuccessMessage, {
+			localVue,
+			propsData: {
+				donation,
+				commentLinkIsDisabled: true,
+			},
+			store: createStore(),
+			mocks: {
+				$t: ( key: string ) => key,
+				$n: () => {},
+			},
+		} );
+
+		expect( wrapper.text() ).toContain( 'donation_confirmation_newsletter_confirmation' );
+	} );
+
+	it( 'does not render direct debit message when payment type is not BEZ', () => {
+		const wrapper = mount( SuccessMessage, {
+			localVue,
+			propsData: {
+				donation: payPalConfirmationData.donation,
+				commentLinkIsDisabled: true,
+			},
+			store: createStore(),
+			mocks: {
+				$t: ( key: string ) => key,
+				$n: () => {},
+			},
+		} );
+
+		expect( wrapper.text() ).not.toContain( 'donation_confirmation_payment_direct_debit' );
+	} );
+
+	it( 'renders direct debit message when payment type is BEZ', () => {
+		const wrapper = mount( SuccessMessage, {
+			localVue,
+			propsData: {
+				donation: directDebitConfirmationData.donation,
+				commentLinkIsDisabled: true,
+			},
+			store: createStore(),
+			mocks: {
+				$t: ( key: string ) => key,
+				$n: () => {},
+			},
+		} );
+
+		expect( wrapper.text() ).toContain( 'donation_confirmation_payment_direct_debit' );
+	} );
+
+	it( 'enables comment link when enabled', () => {
+		const wrapper = mount( SuccessMessage, {
+			localVue,
+			propsData: {
+				donation: payPalConfirmationData.donation,
+				commentLinkIsDisabled: false,
+			},
+			store: createStore(),
+			mocks: {
+				$t: ( key: string ) => key,
+				$n: () => {},
+			},
+		} );
+
+		expect( wrapper.find( '#comment-link' ).element.getAttribute( 'disabled' ) ).toBeFalsy();
+	} );
+
+	it( 'disables comment link when disabled', () => {
+		const wrapper = mount( SuccessMessage, {
+			localVue,
+			propsData: {
+				donation: payPalConfirmationData.donation,
+				commentLinkIsDisabled: true,
+			},
+			store: createStore(),
+			mocks: {
+				$t: ( key: string ) => key,
+				$n: () => {},
+			},
+		} );
+
+		expect( wrapper.find( '#comment-link' ).element.getAttribute( 'disabled' ) ).toBeTruthy();
+	} );
+
+	it( 'emits event when comment link is clicked', () => {
+		const wrapper = mount( SuccessMessage, {
+			localVue,
+			propsData: {
+				donation: payPalConfirmationData.donation,
+				commentLinkIsDisabled: false,
+			},
+			store: createStore(),
+			mocks: {
+				$t: ( key: string ) => key,
+				$n: () => {},
+			},
+		} );
+
+		wrapper.find( '#comment-link' ).trigger( 'click' );
+
+		expect( wrapper.emitted( 'show-comment-modal' ) ).toBeTruthy();
+	} );
+} );
