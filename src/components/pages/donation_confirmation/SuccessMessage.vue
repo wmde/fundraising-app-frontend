d8ef35dd1c4f9effbb34f0af25a4f76380a860fd
Abban Dunne <abban.dunne@wikimedia.de>
2022-09-22 08:59:18 +0200

    Style standard donation success
    Ticket: https://phabricator.wikimedia.org/T316628

--- src/components/pages/donation_confirmation/SuccessMessage.vue
+++ src/components/pages/donation_confirmation/SuccessMessage.vue
@@ -1,9 +1,14 @@
 <template>
 	<div class="donation-confirmation-card has-background-bright has-padding-18 mb-4">
-		<div class="title is-size-5"><success-icon/> {{ $t( 'donation_confirmation_topbox_payment_title_alt' ) }}</div>
+		<h1 class="title icon-title"><success-icon/> {{ $t( 'donation_confirmation_topbox_payment_title_alt' ) }}</h1>
 		<payment-notice :payment="donation"></payment-notice>
 		<div id="newsletter-optin" class="has-margin-top-18" v-if="donation.optsIntoNewsletter">
 			{{ $t( 'donation_confirmation_newsletter_confirmation' ) }}
+		</div>
+		<div class="has-margin-top-18">
+			<a id="comment-link" @click="$emit( 'show-comment-modal' )" v-if="donationCanBeCommented" :disabled="commentLinkIsDisabled">
+				{{ commentLinkIsDisabled ? $t( 'donation_comment_popup_thanks' ) : $t( 'donation_confirmation_comment_button' ) }}
+			</a>
 		</div>
 	</div>
 </template>
@@ -22,6 +27,12 @@ export default Vue.extend( {
 	},
 	props: {
 		donation: Object as () => Donation,
+		commentLinkIsDisabled: Boolean,
+	},
+	computed: {
+		donationCanBeCommented(): boolean {
+			return this.$props.donation.paymentType !== 'UEB';
+		},
 	},
 } );
 </script>
