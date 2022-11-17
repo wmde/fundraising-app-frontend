aecd0e08f2202e479ca1e9bb13a838a656a80436
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-11-17 16:07:52 +0100

    Add alternative headline for receipt opt-out
    https://phabricator.wikimedia.org/T323266

--- src/components/pages/donation_confirmation/AddressKnown.vue
+++ src/components/pages/donation_confirmation/AddressKnown.vue
@@ -1,6 +1,13 @@
 <template>
 	<div class="donation-confirmation-card known-address has-background-bright mb-4">
-		<h2 class="icon-title is-size-5 has-margin-bottom-18"><success-icon/>{{ $t( 'donation_confirmation_summary_title' ) }}</h2>
+		<h2 class="icon-title is-size-5 has-margin-bottom-18">
+			<success-icon/>
+			{{ donation.optsIntoDonationReceipt === true ?
+				$t( 'donation_confirmation_summary_title' ) :
+				$t( 'donation_confirmation_summary_title_no_receipt_wanted' )
+			}}
+		</h2>
+
 		<div class="address-summary">
 			<p v-if="addressType === 'person'" v-html="$t( 'donation_confirmation_address_person', {
 				salutation: salutation,
