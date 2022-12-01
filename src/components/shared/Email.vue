4e44a9616603219c9b5857b5beb4a5e701be4ea2
Sperling-0 <tanuja.doriya@wikimedia.de>
2022-12-01 22:54:25 +0100

    Make email suggestion better
    https://phabricator.wikimedia.org/T321651

--- src/components/shared/Email.vue
+++ src/components/shared/Email.vue
@@ -40,10 +40,12 @@ export default Vue.extend( {
 				return '';
 			}
 			const mailHost = mailUserInput.substring( mailUserInput.lastIndexOf( '@' ) + 1 );
-			const closestFit = closest( mailHost, props.commonMailProviders );
-			const calculatedDistance = distance( mailHost, closestFit );
-			if ( calculatedDistance > 0 && calculatedDistance <= 3 ) {
-				return closestFit;
+			if( mailHost.match( /^\w+\.\w{2}/ ) ) {
+				const closestFit = closest( mailHost, props.commonMailProviders );
+				const calculatedDistance = distance( mailHost, closestFit );
+				if ( calculatedDistance > 0 && calculatedDistance <= 3 ) {
+					return closestFit;
+				}
 			}
 			return '';
 		} );
