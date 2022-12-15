43d29a562845f7de7cf707711407fe3581b91c6f
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-12-15 20:07:34 +0100

    Create design for feedback box
    https://phabricator.wikimedia.org/T325151

--- src/components/pages/DonationForm.vue
+++ src/components/pages/DonationForm.vue
@@ -9,6 +9,7 @@
 				v-bind="currentProperties">
 			</component>
 		</keep-alive>
+		<FeedbackBox v-if="true"/>
 	</div>
 </template>
 
@@ -21,12 +22,14 @@ import { Country } from '@/view_models/Country';
 import { AddressValidation } from '@/view_models/Validation';
 import { Salutation } from '@/view_models/Salutation';
 import { CampaignValues } from '@/view_models/CampaignValues';
+import FeedbackBox from '@/components/pages/donation_form/FeedbackBox.vue';
 
 export default Vue.extend( {
 	name: 'DonationForm',
 	components: {
 		PaymentPage,
 		AddressPage,
+		FeedbackBox,
 	},
 	props: {
 		assetsPath: String,
---
+++ src/components/pages/donation_form/FeedbackBox.vue
@@ -0,0 +1,78 @@
+<template>
+	<div class="feedback-wrapper has-margin-top-0 ">
+
+		<div class="feedback-button">
+
+			<!--
+			// TODO: for mobile:
+				<b-button v-if="!isExpanded"
+					@click="isExpanded = true"
+					type="is-primary is-low"
+					class="button is-primary is-main ">
+					Feedback
+					<b-icon v-if="isOpen" icon="arrow-up" class="icon-size"></b-icon>
+					<b-icon v-else icon="arrow-down" class="icon-size"></b-icon>
+				</b-button>
+				<div>
+				display block, style with blue color
+				</div>
+			-->
+
+			<a v-if="!isExpanded"
+				@click="isExpanded = true"
+				type="is-primary is-low"
+				class="button is-primary is-main ">
+				<span class="text-desktop"> Schwierigkeiten beim Ausfüllen? </span>
+				<span class="text-mobile">
+					Feedback
+					<b-icon v-if="isExpanded" icon="arrow-up" class="icon-size text-mobile"></b-icon>
+					<b-icon v-else icon="arrow-down" class="icon-size text-mobile"></b-icon>
+				</span>
+			</a>
+		</div>
+
+		<div class="feedback-box" v-if="isExpanded">
+			<b-button @click="isExpanded = false" class="feedback-close" >
+				X
+			</b-button>
+			<h3 class="text-desktop">Haben Sie evtl. Schwierigkeiten beim Ausfüllen des Formulars?</h3>
+			<h3 class="text-mobile">Haben Sie Schwierigkeiten?</h3>
+			<p>
+				Wenn Sie Probleme beim Ausfüllen des Spendenformulars haben oder etwas vermissen, dann freuen
+				wir uns über Ihr Feedback.
+			</p>
+
+			<div class="button-group has-margin-top-18" >
+				<b-button @click="isExpanded = false"
+					class="button is-primary">
+					Nein, alles gut
+				</b-button>
+				<a href="https://lime.wikimedia.de/index.php/426896"
+					target="_blank"
+					class="button is-primary">
+					Ja, Feedback geben
+				</a>
+			</div>
+		</div>
+
+		<div v-if="!isExpanded" class="styled-line"/>
+
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend( {
+	name: 'FeedbackBox',
+	props: {
+		isExpanded: Boolean,
+	},
+	data() {
+		return {
+			// isExpanded: this.$props.isExpanded,
+			isExpanded: false,
+		};
+	},
+} );
+</script>
