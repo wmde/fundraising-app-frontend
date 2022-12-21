468012f5ac94a764b422d15da22a91ae12d90dc8
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2022-12-21 14:32:58 +0100

    Wrap feedbackbox in feature toggle functionality
    - also clean up code

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -3,7 +3,12 @@
 		<p><strong>{{ $t('sidebar_getintouch_headline') }}</strong></p>
 		<p v-html="$t('sidebar_getintouch_text')"></p>
 		<p class="has-margin-top-18">{{ $t('sidebar_getintouch_text_phone') }}</p>
-		<FeedbackBox/>
+		<feature-toggle>
+			<div slot="features.feedback-form">
+				<FeedbackBox/>
+			</div>
+		</feature-toggle>
+
 	</aside>
 </template>
 
