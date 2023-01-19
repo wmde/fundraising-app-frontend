4bc23555d8077f53599cca9aa6e27520c9bd7a0e
Abban Dunne <abban.dunne@wikimedia.de>
2023-01-19 12:48:49 +0100

    Update dependencies
    This does not update the Vue dependencies as Buefy
    doesn't support Vue3 yet.

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const ForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' );
-const VueLoaderPlugin = require( 'vue-loader/lib/plugin' );
+const { VueLoaderPlugin } = require( 'vue-loader' );
 const MiniCSSExtractPlugin = require( 'mini-css-extract-plugin' );
 const helpers = require( './helpers' );
 const isDev = process.env.NODE_ENV === 'development';
