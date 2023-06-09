0e9c2f2c42164d54e61c22e849d79c71a41b822d
Sperling-0 <tanujadoriya@gmail.com>
2023-06-09 17:03:21 +0200

    Improve code to fix anonymous and email address type
    Ticket: Ticket: https://phabricator.wikimedia.org/T235017

--- src/components/pages/donation_form/SubmitValues.vue
+++ src/components/pages/donation_form/SubmitValues.vue
@@ -1,36 +1,37 @@
 <template>
 	<span class="submit-values">
-
 		<input type="hidden" name="paymentType" :value="payment.type">
 		<input type="hidden" name="interval" :value="payment.interval">
 		<input type="hidden" name="amount" :value="payment.amount">
+
 		<input type="hidden" name="iban" :value="bankdata.iban">
 		<input type="hidden" name="bic" :value="bankdata.bic">
 
 		<input type="hidden" name="addressType" :value="addressType">
 
-		<template v-if="addressType !== 'anonym'">
-			<template v-if="addressType !== 'email'">
-				<input type="hidden" name="companyName" :value="address.companyName">
-				<input type="hidden" name="street" :value="address.street">
-				<input type="hidden" name="postcode" :value="address.postcode">
-				<input type="hidden" name="city" :value="address.city">
-				<input type="hidden" name="country" :value="address.country">
-			</template>
+		<template v-if="sendNameAndEmail">
 			<input type="hidden" name="salutation" :value="address.salutation">
 			<input type="hidden" name="title" :value="address.title">
 			<input type="hidden" name="firstName" :value="address.firstName">
 			<input type="hidden" name="lastName" :value="address.lastName">
 			<input type="hidden" name="email" :value="address.email">
 		</template>
+
+		<template v-if="sendPostalAddress">
+			<input type="hidden" name="street" :value="address.street">
+			<input type="hidden" name="postcode" :value="address.postcode">
+			<input type="hidden" name="city" :value="address.city">
+			<input type="hidden" name="country" :value="address.country">
+		</template>
+
+		<input type="hidden" name="companyName" :value="address.companyName">
+
 		<input type="hidden" name="info" :value="newsletter">
 		<input type="hidden" name="donationReceipt" :value="receipt">
-
 		<input type="hidden" name="impCount" :value="trackingData.impressionCount">
 		<input type="hidden" name="bImpCount" :value="trackingData.bannerImpressionCount">
 		<input type="hidden" name="piwik_campaign" :value="campaignValues.campaign">
 		<input type="hidden" name="piwik_kwd" :value="campaignValues.keyword">
-
 	</span>
 </template>
 
@@ -53,8 +54,8 @@ export default Vue.extend( {
 		campaignValues: Object as () => CampaignValues,
 	},
 	computed: {
-		addressType() {
-			return addressType;
+		addressType(): string {
+			return addressType.name;
 		},
 		...mapState<Payment>( NS_PAYMENT, {
 			payment: ( state: Payment ) => state.values,
@@ -68,6 +69,12 @@ export default Vue.extend( {
 		...mapState<BankAccount>( NS_BANKDATA, {
 			bankdata: ( state: BankAccount ) => state.values,
 		} ),
+		sendPostalAddress(): boolean {
+			return this.addressType !== 'anonym' && this.addressType !== 'email';
+		},
+		sendNameAndEmail(): boolean {
+			return this.addressType !== 'anonym';
+		},
 		newsletter(): string {
 			return this.$store.getters[ NS_ADDRESS + '/willGetNewsletter' ] ? '1' : '0';
 		},
