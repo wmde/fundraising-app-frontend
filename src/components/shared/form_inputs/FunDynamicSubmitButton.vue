2ec4a0b8579d21ded3c50a133e5c4ec3704b8fdd
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2023-09-11 14:42:04 +0200

    Change setup() to script setup tag

--- src/components/shared/form_inputs/FunDynamicSubmitButton.vue
+++ src/components/shared/form_inputs/FunDynamicSubmitButton.vue
@@ -8,37 +8,30 @@
 	</button>
 </template>
 
-<script lang="ts">
-import { computed, defineComponent, PropType } from 'vue';
+<script setup lang="ts">
+import { computed } from 'vue';
 
-export default defineComponent( {
-	name: 'FunDynamicTextButton',
-	props: {
-		paymentType: String,
-		buttonType: {
-			type: String as PropType<'button'|'submit'|'reset'>,
-			default: 'button',
-		},
-	},
-	setup( props: any ) {
-		const dynamicButtonText = computed( () => {
-			switch ( props.paymentType ) {
-				case 'PPL':
-					return 'donation_form_finalize_paypal';
-				case 'UEB':
-					return 'donation_form_finalize_bank_transfer';
-				case 'MCP':
-					return 'donation_form_finalize_credit_card';
-				case 'SUB':
-					return 'donation_form_finalize_sofort';
-				default:
-					return 'donation_form_finalize';
-			}
-		} );
+interface Props {
+	paymentType: 'PPL' | 'UEB' | 'MCP' | 'SUB';
+	buttonType?: 'button'|'submit'|'reset';
+}
 
-		return {
-			dynamicButtonText,
-		};
-	},
+const props = withDefaults( defineProps<Props>(), {
+	buttonType: 'button',
+} );
+
+const dynamicButtonText = computed( () => {
+	switch ( props.paymentType ) {
+		case 'PPL':
+			return 'donation_form_finalize_paypal';
+		case 'UEB':
+			return 'donation_form_finalize_bank_transfer';
+		case 'MCP':
+			return 'donation_form_finalize_credit_card';
+		case 'SUB':
+			return 'donation_form_finalize_sofort';
+		default:
+			return 'donation_form_finalize';
+	}
 } );
 </script>
