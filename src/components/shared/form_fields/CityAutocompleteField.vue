2fc2f26183a9d8a0d00a805c930309778e3fb6f4
Abban Dunne <abban.dunne@wikimedia.de>
2023-09-12 16:12:19 +0200

    Add new City Autocomplete field
    Ticket: https://phabricator.wikimedia.org/T345528

---
+++ src/components/shared/form_fields/CityAutocompleteField.vue
@@ -0,0 +1,79 @@
+<template>
+	<div class="form-field form-field-autocomplete" :class="{ 'is-invalid': showError }">
+		<label for="city" class="form-field-label">{{ label }}</label>
+		<div class="form-field-autocomplete-container">
+			<TextFormInput
+				v-model="city"
+				input-type="text"
+				name="city"
+				:placeholder="$t( placeholder, { example: $t( examplePlaceholder ) } )"
+				:has-error="showError"
+				:has-message="false"
+				input-id="city"
+				@focus="onFocus"
+				@blur="onBlur"
+			/>
+			<transition name="fade">
+				<div class="dropdown-menu" v-show="autocompleteIsActive">
+					<div class="dropdown-content">
+						<template v-for="city in cities">
+							<a class="dropdown-item" role="button" tabindex="0" @click.stop="onSelectItem( city )">
+								<strong>{{ postcode }}</strong> {{ city }}
+							</a>
+						</template>
+					</div>
+				</div>
+			</transition>
+		</div>
+		<span v-if="showError" class="help is-danger">{{ errorMessage }}</span>
+		<slot name="message"/>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { computed, inject, onMounted, ref, watch } from 'vue';
+import { useCitiesResource } from '@src/components/shared/form_inputs/useCitiesResource';
+import { CityAutocompleteResource, NullCityAutocompleteResource } from '@src/CityAutocompleteResource';
+import { useAutocompleteEvents } from '@src/components/shared/form_fields/useAutocompleteEvents';
+import TextFormInput from '@src/components/shared/form_inputs/TextFormInput.vue';
+
+interface Props {
+	modelValue: string;
+	label: String;
+	examplePlaceholder: string;
+	showError: boolean;
+	errorMessage: String;
+	postcode: string;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits( [ 'field-changed', 'update:modelValue' ] );
+
+const city = ref<string>( props.modelValue );
+const { autocompleteIsActive, onFocus, onBlur, onSelectItem } = useAutocompleteEvents( city, emit );
+const { cities, fetchCitiesForPostcode } = useCitiesResource( inject<CityAutocompleteResource>( 'cityAutocompleteResource', NullCityAutocompleteResource ) );
+
+const placeholder = computed( () => {
+	if ( cities.value.length > 0 ) {
+		return 'form_autocomplete_prompt';
+	}
+	return 'form_for_example';
+} );
+
+onMounted( () => {
+	fetchCitiesForPostcode( props.postcode as string );
+} );
+
+watch( (): string => props.postcode as string, ( value: string ) => {
+	fetchCitiesForPostcode( value );
+} );
+
+watch( city, ( newCity: string ) => {
+	emit( 'update:modelValue', newCity );
+} );
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
