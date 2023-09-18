348a34b031aaa32077e027f8c94162ba31774888
Abban Dunne <abban.dunne@wikimedia.de>
2023-09-18 09:55:36 +0200

    Convert donation payment to composition API

--- src/components/pages/donation_form/Payment.vue
+++ src/components/pages/donation_form/Payment.vue
@@ -1,13 +1,13 @@
 <template>
 	<div class="payment-section">
-		<amount-selection
+		<AmountSelection
 			:payment-amounts="paymentAmounts"
 			:amount="amount"
 			:title="$t('donation_form_payment_amount_title')"
-			:error="showErrorMessage"
+			:error="showAmountErrorMessage"
 			v-on:amount-selected="sendAmountToStore"
 		/>
-		<payment-interval
+		<PaymentInterval
 			class="has-margin-top-36"
 			:payment-intervals="paymentIntervals"
 			:current-interval="interval"
@@ -15,11 +15,11 @@
 			:disabled-payment-intervals="disabledPaymentIntervals"
 			v-on:interval-selected="sendIntervalToStore"
 		/>
-		<payment-type
+		<PaymentType
 			class="has-margin-top-36"
-			:current-type="type"
+			:current-type="paymentType"
 			:payment-types="paymentTypes"
-			:error="typeIsValid ? '' : $t('donation_form_payment_type_error')"
+			:error="paymentTypeIsValid ? '' : $t('donation_form_payment_type_error')"
 			:title="$t('donation_form_payment_type_title')"
 			:disabled-payment-types="disabledPaymentTypes"
 			v-on:payment-type-selected="sendTypeToStore"
@@ -27,77 +27,69 @@
 	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { computed } from 'vue';
 import AmountSelection from '@src/components/shared/AmountSelection.vue';
 import PaymentInterval from '@src/components/shared/PaymentInterval.vue';
 import PaymentType from '@src/components/pages/donation_form/PaymentType.vue';
 import { action } from '@src/store/util';
 import { NS_ADDRESS, NS_PAYMENT } from '@src/store/namespaces';
 import { setAmount, setInterval, setType } from '@src/store/payment/actionTypes';
-import { mapGetters, mapState } from 'vuex';
+import { useStore } from 'vuex';
 import { AddressTypeModel } from '@src/view_models/AddressTypeModel';
 import { AmountValidity } from '@src/view_models/Payment';
+import { useI18n } from 'vue-i18n';
 
-export default defineComponent( {
-	name: 'Payment',
-	components: {
-		AmountSelection,
-		PaymentInterval,
-		PaymentType,
-	},
-	props: [ 'paymentAmounts', 'paymentIntervals', 'paymentTypes' ],
-	computed: {
-		...mapState( {
-			amount: ( state: any ) => state[ NS_PAYMENT ].values.amount,
-			interval: ( state: any ) => state[ NS_PAYMENT ].values.interval,
-			type: ( state: any ) => state[ NS_PAYMENT ].values.type,
-			disabledPaymentTypes: ( state: any ) => {
-				var disabledTypes : String[] = [];
-				if ( state[ NS_ADDRESS ].addressType === AddressTypeModel.ANON ) {
-					disabledTypes.push( 'BEZ' );
-				}
-				if ( state[ NS_PAYMENT ].values.interval !== '0' ) {
-					disabledTypes.push( 'SUB' );
-				}
-				return disabledTypes;
-			},
-			emailOnlyDisabledPaymentTypes: ( state: any ) => {
-				if ( state[ NS_PAYMENT ].values.interval !== '0' ) {
-					return [ 'SUB' ];
-				}
-				return [];
-			},
-			disabledPaymentIntervals: function ( state: any ) {
-				var disabledIntervals : String[] = [];
-				if ( state[ NS_PAYMENT ].values.type === 'SUB' ) {
-					disabledIntervals = ( this as any ).$props.paymentIntervals
-						.filter( ( interval:Number ) => Number( interval ) > 0 )
-						.map( ( interval:Number ) => String( interval ) );
-				}
-				return disabledIntervals;
-			},
-		} ),
-		...mapGetters( NS_PAYMENT, [ 'amountValidity', 'typeIsValid' ] ),
-		showErrorMessage(): String {
-			const messages : { [ key:number ]:string; } = {
-				[ AmountValidity.AMOUNT_VALID ]: '',
-				[ AmountValidity.AMOUNT_TOO_LOW ]: this.$t( 'donation_form_payment_amount_error' ) as string,
-				[ AmountValidity.AMOUNT_TOO_HIGH ]: this.$t( 'donation_form_payment_amount_too_high' ) as string,
-			};
-			return messages[ this.$store.getters[ NS_PAYMENT + '/amountValidity' ] ];
-		},
-	},
-	methods: {
-		sendAmountToStore( amountValue: string ): void {
-			this.$store.dispatch( action( NS_PAYMENT, setAmount ), amountValue );
-		},
-		sendIntervalToStore( interval: string ): void {
-			this.$store.dispatch( action( NS_PAYMENT, setInterval ), interval );
-		},
-		sendTypeToStore( paymentType: string ): void {
-			this.$store.dispatch( action( NS_PAYMENT, setType ), paymentType );
-		},
-	},
+interface Props {
+	paymentAmounts: number[];
+	paymentIntervals: number[];
+	paymentTypes: string[];
+}
+
+defineProps<Props>();
+
+const store = useStore();
+const { t } = useI18n();
+
+const amount = computed<number>( () => store.state[ NS_PAYMENT ].values.amount );
+const interval = computed<string>( () => store.state[ NS_PAYMENT ].values.interval );
+const paymentType = computed<string>( () => store.state[ NS_PAYMENT ].values.type );
+const paymentTypeIsValid = computed<boolean>( () => store.state[ NS_PAYMENT ].validity.type );
+const disabledPaymentTypes = computed<string[]>( () => {
+	let disabledTypes : string[] = [];
+	if ( store.state[ NS_ADDRESS ].addressType === AddressTypeModel.ANON ) {
+		disabledTypes.push( 'BEZ' );
+	}
+	if ( store.state[ NS_PAYMENT ].values.interval !== '0' ) {
+		disabledTypes.push( 'SUB' );
+	}
+	return disabledTypes;
 } );
+const disabledPaymentIntervals = computed<string[]>( () => {
+	let disabledIntervals : string[] = [];
+	if ( store.state[ NS_PAYMENT ].values.type === 'SUB' ) {
+		disabledIntervals = ( this as any ).$props.paymentIntervals
+			.filter( ( x: number ) => Number( x ) > 0 )
+			.map( ( x: number ) => String( x ) );
+	}
+	return disabledIntervals;
+} );
+const showAmountErrorMessage = computed<String>( () => {
+	const messages : { [ key:number ]:string; } = {
+		[ AmountValidity.AMOUNT_VALID ]: '',
+		[ AmountValidity.AMOUNT_TOO_LOW ]: t( 'donation_form_payment_amount_error' ),
+		[ AmountValidity.AMOUNT_TOO_HIGH ]: t( 'donation_form_payment_amount_too_high' ),
+	};
+	return messages[ store.getters[ NS_PAYMENT + '/amountValidity' ] ];
+} );
+
+const sendAmountToStore = ( newAmount: string ): void => {
+	store.dispatch( action( NS_PAYMENT, setAmount ), newAmount );
+};
+const sendIntervalToStore = ( newInterval: string ): void => {
+	store.dispatch( action( NS_PAYMENT, setInterval ), newInterval );
+};
+const sendTypeToStore = ( newPaymentType: string ): void => {
+	store.dispatch( action( NS_PAYMENT, setType ), newPaymentType );
+};
 </script>
