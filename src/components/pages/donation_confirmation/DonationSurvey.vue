e322fd4284617c55c3e5fd5b5038391d7d3fc199
Sperling-0 <tanujadoriya@gmail.com>
2023-11-03 08:42:42 +0100

    Add donation survey tile to donation confirmation page.
    The donation survey tile asks users to participate in a short survey.
    The link in the tile leads to either the German or the English version of the survey.
    Copy and link targets are in the content repository.
    
    Change snake case to Pascal case for using Vue components in template
    
    Ticket: https://phabricator.wikimedia.org/T349193

---
+++ src/components/pages/donation_confirmation/DonationSurvey.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="donation-confirmation-card">
+		<h2 class="icon-title"><donor-icon/> {{ $t( 'donation_confirmation_survey_title' ) }}</h2>
+		<p>{{ $t( 'donation_confirmation_survey_content' ) }}</p>
+		<p>{{ $t( 'donation_confirmation_survey_link_text' ) }}</p>
+		<p><a :href="$t( 'donation_confirmation_survey_link' )">{{ $t( 'donation_confirmation_survey_link' ) }}</a></p>
+	</div>
+</template>
+
+<script setup lang="ts">
+import DonorIcon from '@src/components/shared/icons/DonorIcon.vue';
+</script>
