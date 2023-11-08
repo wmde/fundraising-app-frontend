176e1da3497ed3b377d9a55bd43a8531e7f9d810
Abban Dunne <abban.dunne@wikimedia.de>
2023-11-08 13:37:26 +0100

    Fix bank message columns

--- src/components/pages/donation_confirmation/SuccessMessageBankTransfer.vue
+++ src/components/pages/donation_confirmation/SuccessMessageBankTransfer.vue
@@ -1,17 +1,17 @@
 <template>
 	<div class="donation-confirmation-card success-message-bank-transfer">
-		<div class="columns is-multiline">
-			<div class="column is-full">
+		<div class="columns is-multiline pt-0 pb-0 mt-0 mb-0">
+			<div class="column is-full pt-0 pb-0 mt-0 mb-0">
 				<h1 class="icon-title"><warning-icon/> {{ $t( 'donation_confirmation_topbox_payment_title_bank_transfer_alt' ) }}</h1>
 			</div>
-			<div class="column is-half">
+			<div class="column is-half pt-0 pb-0 mt-0 mb-0">
 				<p>{{ donationSummaryMessage }}</p>
 				<p v-html="$t( 'donation_confirmation_reminder_bank_transfer', { bankTransferCode: donation.bankTransferCode } )"/>
 				<p id="newsletter-box" v-if="donation.newsletter">
 					{{ $t( 'donation_confirmation_newsletter_confirmation' ) }}
 				</p>
 			</div>
-			<div class="column is-half">
+			<div class="column is-half pt-0 pb-0 mt-0 mb-0">
 				<BankData :bank-transfer-code="donation.bankTransferCode"/>
 			</div>
 		</div>
@@ -44,12 +44,19 @@ export default defineComponent( {
 </script>
 
 <style lang="scss">
+@use 'src/scss/settings/units';
+@use 'sass:map';
+
 .success-message-bank-transfer {
 	.bank-data-content {
 		background: #DDEDF6;
 		padding: 1rem;
 		display: inline-block;
 		list-style-type: none;
+	}
+
+	p:last-child {
+		margin-bottom: map.get( units.$spacing, 'small' );
 	}
 }
 </style>
