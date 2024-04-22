651257dd7052e0c03dff8c5432738e933bfddc87
Sperling-0 <tanujadoriya@gmail.com>
2024-04-22 18:12:44 +0200

    Allow keyboard navigation for selecting the language
    Ticket: https://phabricator.wikimedia.org/T361781
    
    Co-authored-by: Gabriel Birke <gabriel.birke@wikimedia.de>

--- src/components/shared/LocaleSelector.vue
+++ src/components/shared/LocaleSelector.vue
@@ -1,33 +1,48 @@
 <template>
-	<div class="navigation-locale" role="button">
-
-		<div class="navigation-locale-current">
+	<div class="navigation-locale">
+		<button class="navigation-locale-current"
+			type="button"
+			id="menubutton1"
+			aria-haspopup="true"
+			:aria-expanded="popUpIsOpen"
+			aria-controls="menu1"
+			ref="buttonNode"
+			@keydown="onButtonKeydown"
+			@click="onButtonClick"
+		>
 			{{ locale === 'de_DE' ? 'de' : 'en' }} <ChevronDown/>
-		</div>
-
-		<div class="navigation-locale-dropdown">
-			<a :class="{ 'active': locale === 'de_DE' }"
-					role="link"
-					tabindex="0"
-					@click="setCookie( 'de_DE' )"
-					@keyup.enter.space="setCookie( 'de_DE')"
+		</button>
+			<ul class="navigation-locale-dropdown"
+				id="menu1"
+				role="menu"
+				tabindex="-1"
+				aria-labelledby="menubutton1"
+				:aria-activedescendant="selectedMenuItem"
+				ref="menuNode"
+				v-show="popUpIsOpen"
+				@keydown="onMenuKeydown"
 			>
-				Deutsch <Checkmark v-if="locale === 'de_DE'"/>
-			</a>
-			<a :class="{ 'active': locale === 'en_GB' }"
-					role="link"
-					tabindex="0"
-					@click="setCookie( 'en_GB' )"
-					@keyup.enter.space="setCookie( 'en_GB')"
-			>
-				English <Checkmark v-if="locale === 'en_GB'"/>
-			</a>
-		</div>
+				<li v-for="( item, index ) in menuItems"
+					:key="index"
+					:id="'mi' + (index + 1)"
+					role="menuitem"
+					:class="{ 'active': selectedIndex === index }"
+					:tabindex="selectedIndex === index ? 0 : -1"
+					@keydown="onMenuKeydown"
+					@click="onMenuitemClick"
+					@mouseover="onMenuitemMouseover"
+					:data-locale="item.locale"
+				>
+					<a :class="{ 'active': locale === item.locale, 'focus': focusedIndex === index }">
+						{{ item.label }} <Checkmark v-if="locale === item.locale"/>
+					</a>
+				</li>
+			</ul>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import Cookies from 'js-cookie';
 import { COOKIE_NAME, DEFAULT_LOCALE } from '@src/util/createLocalisation';
 import ChevronDown from '@src/components/shared/icons/ChevronDown.vue';
@@ -35,11 +50,215 @@ import Checkmark from '@src/components/shared/icons/Checkmark.vue';
 
 const locale = ref<string>( Cookies.get( COOKIE_NAME ) ?? DEFAULT_LOCALE );
 
-const setCookie = ( value: string ): void => {
+const menuItems = ref( [
+	{ locale: 'de_DE', label: 'Deutsch' },
+	{ locale: 'en_GB', label: 'English' },
+] );
+
+const setLocale = ( value: string ): void => {
 	locale.value = value;
 	Cookies.set( COOKIE_NAME, value );
 	window.location.reload();
 };
+
+const buttonNode = ref( null );
+const menuNode = ref( null );
+const popUpIsOpen = ref( false );
+const selectedMenuItem = ref( '' );
+const selectedIndex = computed( () =>
+	menuItems.value.findIndex( ( currentItem ) => currentItem.locale === locale.value )
+);
+const focusedIndex = ref( -1 );
+
+const closePopup = () => {
+	if ( popUpIsOpen.value === true ) {
+		popUpIsOpen.value = false;
+		buttonNode.value.focus();
+	}
+};
+
+const setFocusToFirstMenuitem = () => {
+	focusedIndex.value = 0;
+};
+
+const openPopup = () => {
+	popUpIsOpen.value = true;
+	menuNode.value.focus();
+};
+
+const setFocusToLastMenuitem = () => {
+	focusedIndex.value = menuItems.value.length - 1;
+};
+
+const setFocusToPreviousMenuitem = () => {
+	focusedIndex.value = focusedIndex.value === 0 ? menuItems.value.length - 1 : focusedIndex.value - 1;
+};
+
+const setFocusToNextMenuitem = () => {
+	focusedIndex.value = focusedIndex.value === menuItems.value.length - 1 ? 0 : focusedIndex.value + 1;
+};
+
+const setFocusByFirstCharacter = ( char: string ) => {
+	const index = menuItems.value.findIndex(
+		( currentItem ) => currentItem.label.slice( 0, 1 ).toLowerCase() === char.toLowerCase()
+	);
+	if ( index !== -1 ) {
+		focusedIndex.value = index;
+	}
+};
+
+const onButtonKeydown = ( event: KeyboardEvent ) => {
+	const key = event.key;
+	let flag = false;
+
+	switch ( key ) {
+		case ' ':
+		case 'Enter':
+		case 'ArrowDown':
+		case 'Down':
+			openPopup();
+			setFocusToFirstMenuitem();
+			flag = true;
+			break;
+
+		case 'Esc':
+		case 'Escape':
+			closePopup();
+			flag = true;
+			break;
+
+		case 'Up':
+		case 'ArrowUp':
+			openPopup();
+			setFocusToLastMenuitem();
+			flag = true;
+			break;
+
+		default:
+			break;
+	}
+
+	if ( flag ) {
+		event.stopPropagation();
+		event.preventDefault();
+	}
+};
+
+const onButtonClick = ( event: MouseEvent ) => {
+	if ( popUpIsOpen.value === true ) {
+		closePopup();
+	} else {
+		openPopup();
+		setFocusToFirstMenuitem();
+	}
+	event.stopPropagation();
+	event.preventDefault();
+};
+
+const onMenuKeydown = ( event: KeyboardEvent ) => {
+	const key = event.key;
+	let flag = false;
+
+	function isPrintableCharacter( str: string ) {
+		return str.length === 1 && str.match( /\S/ );
+	}
+
+	if ( event.ctrlKey || event.altKey || event.metaKey ) {
+		return;
+	}
+
+	if ( event.shiftKey ) {
+		if ( isPrintableCharacter( key ) ) {
+			setFocusByFirstCharacter( key );
+			flag = true;
+		}
+
+		if ( event.key === 'Tab' ) {
+			closePopup();
+			flag = true;
+		}
+	} else {
+		switch ( key ) {
+			case ' ':
+			case 'Enter':
+				closePopup();
+				setLocale( menuItems.value[ focusedIndex.value ].locale );
+				flag = true;
+				break;
+
+			case 'Esc':
+			case 'Escape':
+				closePopup();
+				flag = true;
+				break;
+
+			case 'Up':
+			case 'ArrowUp':
+				setFocusToPreviousMenuitem();
+				flag = true;
+				break;
+
+			case 'ArrowDown':
+			case 'Down':
+				setFocusToNextMenuitem();
+				flag = true;
+				break;
+
+			case 'Home':
+			case 'PageUp':
+				setFocusToFirstMenuitem();
+				flag = true;
+				break;
+
+			case 'End':
+			case 'PageDown':
+				setFocusToLastMenuitem();
+				flag = true;
+				break;
+
+			case 'Tab':
+				closePopup();
+				break;
+
+			default:
+				if ( isPrintableCharacter( key ) ) {
+					setFocusByFirstCharacter( key );
+					flag = true;
+				}
+				break;
+		}
+	}
+
+	if ( flag ) {
+		event.stopPropagation();
+		event.preventDefault();
+	}
+};
+
+const onMenuitemMouseover = ( event: MouseEvent ) => {
+	const target = event.currentTarget as HTMLElement;
+	focusedIndex.value = menuItems.value.findIndex(
+		( currentItem ) => currentItem.locale === target.getAttribute( 'data-locale' )
+	);
+};
+
+const onMenuitemClick = ( event: MouseEvent ) => {
+	const target = event.currentTarget as HTMLElement;
+	closePopup();
+	setLocale( target.getAttribute( 'data-locale' ) );
+};
+
+const onBackgroundMousedown = ( event: MouseEvent ) => {
+	if ( !buttonNode.value.contains( event.target ) ) {
+		if ( popUpIsOpen.value === true ) {
+			closePopup();
+		}
+	}
+};
+
+onMounted( ()=> {
+	document.body.addEventListener( 'click', onBackgroundMousedown );
+} );
 
 </script>
 
@@ -55,6 +274,8 @@ const setCookie = ( value: string ): void => {
 	width: global.$navbar-height;
 
 	&-current {
+		background: colors.$white;
+		border: none;
 		height: 100%;
 		display: flex;
 		justify-content: center;
@@ -62,10 +283,11 @@ const setCookie = ( value: string ): void => {
 		cursor: pointer;
 		color: colors.$primary;
 		font-weight: bold;
+		text-transform: uppercase;
 
 		svg {
 			width: 10px;
-			margin-left: 4px;
+			margin-left: 5px;
 			margin-top: 3px;
 			path {
 				stroke: colors.$primary;
@@ -74,8 +296,6 @@ const setCookie = ( value: string ): void => {
 	}
 
 	&-dropdown {
-		visibility: hidden;
-		opacity: 0;
 		position: absolute;
 		background: colors.$white;
 		top: 80%;
@@ -92,14 +312,10 @@ const setCookie = ( value: string ): void => {
 			color: colors.$primary;
 
 			&:hover,
-			&:focus {
+			&:focus,
+			&.focus {
 				background: colors.$gray-light;
 				text-decoration: none;
-			}
-
-			&.active {
-				background: colors.$primary-locale-active;
-				color: colors.$primary;
 			}
 
 			svg {
@@ -112,14 +328,6 @@ const setCookie = ( value: string ): void => {
 					fill: colors.$black;
 				}
 			}
-		}
-	}
-
-	&:hover,
-	&:focus {
-		.navigation-locale-dropdown {
-			opacity: 1;
-			visibility: visible;
 		}
 	}
 }
--- tests/unit/components/shared/LocaleSelector.spec.ts
+++ tests/unit/components/shared/LocaleSelector.spec.ts
@@ -10,7 +10,7 @@ describe( 'LocaleSelector.vue', () => {
 		// @ts-ignore
 		jest.spyOn( Cookies, 'get' ).mockReturnValue( undefined );
 
-		const wrapper = mount( LocaleSelector, { props: { assetsPath: '' } } );
+		const wrapper = mount( LocaleSelector );
 
 		expect( ( wrapper.vm as any ).locale ).toEqual( DEFAULT_LOCALE );
 	} );
@@ -21,7 +21,7 @@ describe( 'LocaleSelector.vue', () => {
 		// @ts-ignore
 		jest.spyOn( Cookies, 'get' ).mockReturnValue( cookieValue );
 
-		const wrapper = mount( LocaleSelector, { props: { assetsPath: '' } } );
+		const wrapper = mount( LocaleSelector );
 
 		expect( ( wrapper.vm as any ).locale ).toEqual( cookieValue );
 	} );
@@ -42,9 +42,9 @@ describe( 'LocaleSelector.vue', () => {
 				return undefined;
 			} );
 
-		const wrapper = mount( LocaleSelector, { props: { assetsPath: '' } } );
+		const wrapper = mount( LocaleSelector );
 
-		( wrapper.vm as any ).setCookie( cookieValue );
+		( wrapper.vm as any ).setLocale( cookieValue );
 
 		expect( savedKey ).toEqual( COOKIE_NAME );
 		expect( savedValue ).toEqual( cookieValue );
@@ -55,10 +55,111 @@ describe( 'LocaleSelector.vue', () => {
 			value: { reload: jest.fn() },
 		} );
 
-		const wrapper = mount( LocaleSelector, { props: { assetsPath: '' } } );
+		const wrapper = mount( LocaleSelector );
 
-		( wrapper.vm as any ).setCookie( 'cookieValueOfTheLivingDead' );
+		( wrapper.vm as any ).setLocale( 'cookieValueOfTheLivingDead' );
 
 		expect( window.location.reload ).toBeCalled();
+	} );
+
+	it.each( [
+		[ 'Enter' ],
+		[ ' ' ],
+		[ 'ArrowUp' ],
+		[ 'ArrowDown' ],
+	] )( 'interacting with language indicator via key-board shows or hides drop-down', async ( keyFromUser: string ) => {
+		const wrapper = mount( LocaleSelector );
+		const button = wrapper.find( '.navigation-locale-current' );
+		const dropdown = wrapper.find( '.navigation-locale-dropdown' );
+
+		expect( button.attributes( 'aria-expanded' ) ).toBe( 'false' );
+		expect( dropdown.isVisible() ).toBeFalsy();
+
+		await button.trigger( 'keydown', { key: keyFromUser } );
+
+		expect( button.attributes( 'aria-expanded' ) ).toBe( 'true' );
+		expect( dropdown.isVisible() ).toBeTruthy();
+
+		await button.trigger( 'keydown', { key: 'Escape' } );
+
+		expect( button.attributes( 'aria-expanded' ) ).toBe( 'false' );
+		expect( dropdown.isVisible() ).toBeFalsy();
+	} );
+
+	it( 'interacting with language indicator via mouse shows or hides drop-down', async () => {
+		const wrapper = mount( LocaleSelector );
+		const dropdown = wrapper.find( '.navigation-locale-dropdown' );
+		const button = wrapper.find( '.navigation-locale-current' );
+
+		expect( button.attributes( 'aria-expanded' ) ).toBe( 'false' );
+		expect( dropdown.isVisible() ).toBeFalsy();
+
+		await button.trigger( 'click' );
+
+		expect( button.attributes( 'aria-expanded' ) ).toBe( 'true' );
+		expect( dropdown.isVisible() ).toBeTruthy();
+
+		await button.trigger( 'click' );
+
+		expect( button.attributes( 'aria-expanded' ) ).toBe( 'false' );
+		expect( dropdown.isVisible() ).toBeFalsy();
+	} );
+
+	it.each( [
+		[ 'Enter', 1 ],
+		[ ' ', 1 ],
+		[ 'ArrowUp', 2 ],
+		[ 'ArrowDown', 1 ],
+	] )( 'interacting with language indicator via keyboard changes the focus in drop-down options', async ( keyFromUser: string, focusedIndex: number ) => {
+		const wrapper = mount( LocaleSelector );
+		const button = wrapper.find( '.navigation-locale-current' );
+
+		await button.trigger( 'keydown', { key: keyFromUser } );
+
+		const focusedItem = wrapper.find( `.navigation-locale-dropdown li:nth-child(${focusedIndex}) a.focus` );
+		expect( focusedItem.exists() ).toBeTruthy();
+	} );
+
+	it( 'closes the popup when Shift + Tab is pressed', async () => {
+		const wrapper = mount( LocaleSelector );
+		const dropdown = wrapper.find( '.navigation-locale-dropdown' );
+		const button = wrapper.find( '.navigation-locale-current' );
+
+		await button.trigger( 'click' );
+
+		expect( dropdown.isVisible() ).toBeTruthy();
+
+		await wrapper.find( '.navigation-locale-dropdown li:last-child' ).trigger( 'keydown', { key: 'Tab', shiftKey: true } );
+
+		expect( dropdown.isVisible() ).toBeFalsy();
+	} );
+
+	it.each( [
+		[ 'Enter' ],
+		[ ' ' ],
+	] )( 'selecting an item in the drop-down stores the locale and triggers reload', async ( keyFromUser ) => {
+		let savedValue: string | object = '';
+		Object.defineProperty( window, 'location', {
+			value: { reload: jest.fn() },
+		} );
+
+		jest.spyOn( Cookies, 'set' )
+			.mockImplementation( ( name: string, value: string | object ): string | undefined => {
+				savedValue = value;
+				return undefined;
+			} );
+
+		const wrapper = mount( LocaleSelector );
+		const menu = wrapper.find( '.navigation-locale-dropdown' );
+		const button = wrapper.find( '.navigation-locale-current' );
+
+		const event = { key: keyFromUser };
+
+		await button.trigger( 'click' );
+		await menu.trigger( 'keydown', event );
+
+		expect( menu.isVisible() ).toBeFalsy();
+		expect( savedValue ).toBe( 'de_DE' );
+		expect( window.location.reload ).toHaveBeenCalled();
 	} );
 } );
