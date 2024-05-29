7630c2c905d820f9a28bd93054f757e33cfcd2e9
Abban Dunne <abban.dunne@wikimedia.de>
2024-05-29 07:41:29 +0200

    Update Donation Confirmation More Info
    - Convert it to a details summary element
    - Moved styles into the component
    - Fixed another instance where Bulma was defaulting colours
      this time link hover colours were very dark grey
    - Added mailto link to email address in summary text
    
    Ticket: https://phabricator.wikimedia.org/T365464

--- src/components/pages/donation_confirmation/AddressUsageToggle.vue
+++ src/components/pages/donation_confirmation/AddressUsageToggle.vue
@@ -1,17 +1,66 @@
 <template>
-    <div class="address-usage-toggle" :class="{ 'open': contentOpen }">
-		<a href="#" @click.prevent="toggleContent" class="address-usage-toggle-link">{{ $t('donation_confirmation_address_usage_link') }}</a>
-		<div class="address-usage-toggle-content">{{ $t('donation_confirmation_address_usage_content') }}</div>
-    </div>
+	<details class="address-usage-toggle">
+		<summary class="address-usage-toggle-link">{{ $t( 'donation_confirmation_address_usage_link' ) }}</summary>
+		<div class="address-usage-toggle-content" v-html="$t( 'donation_confirmation_address_usage_content' )"/>
+	</details>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
+<style lang="scss">
+@use "@src/scss/settings/colors";
 
-const contentOpen = ref<boolean>( false );
+.address-usage-toggle {
+	&-link {
+		color: colors.$black;
+		position: relative;
+		padding-left: 24px;
+		padding-top: 5px;
+		cursor: pointer;
+		font-size: 12px;
+		list-style-type: none;
 
-const toggleContent = (): void => {
-	contentOpen.value = !contentOpen.value;
-};
+		&::-webkit-details-marker {
+			display: none;
+		}
 
-</script>
+		&:hover {
+			text-decoration: underline;
+			color: colors.$primary;
+		}
+
+		&::after {
+			border: 2px solid colors.$primary;
+			border-radius: 2px;
+			border-right: 0;
+			border-top: 0;
+			content: ' ';
+			display: block;
+			margin-top: -0.6em;
+			pointer-events: none;
+			position: absolute;
+			top: 50%;
+			left: 5px;
+			transform: rotate(-45deg);
+			transform-origin: center;
+			height: 1em;
+			width: 1em;
+
+			transition: transform 100ms, margin-top 100ms;
+		}
+	}
+
+	&-content {
+		font-size: 14px;
+	}
+
+	&[open] {
+		.address-usage-toggle-link::after {
+			transform: rotate(135deg);
+			margin-top: -0.3em;
+		}
+
+		.address-usage-toggle-content {
+			display: block;
+		}
+	}
+}
+</style>
