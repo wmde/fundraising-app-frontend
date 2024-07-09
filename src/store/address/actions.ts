fa0579fc28c6a704ba238ae5af3e7f86cc80f652
Abban Dunne <abban.dunne@wikimedia.de>
2024-07-09 14:11:41 +0200

    Remove unused store address validateCountry method

--- src/store/address/actions.ts
+++ src/store/address/actions.ts
@@ -8,13 +8,11 @@ import {
 	validateAddress,
 	validateAddressField,
 	validateAddressType,
-	validateCountry,
 	validateEmail,
 } from '@src/store/address/actionTypes';
 import {
 	AddressState,
 	AddressTypeValidationRequest,
-	CountryValidationFields,
 	InitialAddressValues,
 	InputField,
 } from '@src/view_models/Address';
@@ -43,12 +41,6 @@ export const actions = {
 		field.value = field.value.trim();
 		context.commit( SET_ADDRESS_FIELD, field );
 		context.commit( VALIDATE_INPUT, field );
-	},
-	[ validateCountry ]( context: ActionContext<AddressState, any>, countryValidation: CountryValidationFields ) {
-		context.commit( SET_ADDRESS_FIELD, countryValidation.country );
-		context.commit( VALIDATE_INPUT, countryValidation.country );
-		context.commit( SET_ADDRESS_FIELD, countryValidation.postcode );
-		context.commit( VALIDATE_INPUT, countryValidation.postcode );
 	},
 	[ validateAddress ]( context: ActionContext<AddressState, any>, validateAddressUrl: string ) {
 		context.commit( MARK_EMPTY_FIELDS_INVALID );
