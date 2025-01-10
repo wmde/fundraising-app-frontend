8fa91e258b6d22b8a4d9ebe06cca3267b3a60471
Gabriel Birke <gabriel.birke@gmail.com>
2025-01-10 10:43:59 +0100

    Apply ESLint rules to ESLint config
    For more consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,29 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import vuejsAccessibility from "eslint-plugin-vuejs-accessibility";
-import pluginVue from 'eslint-plugin-vue'
-import globals from "globals";
-import parser from "vue-eslint-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import vuejsAccessibility from 'eslint-plugin-vuejs-accessibility';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import parser from 'vue-eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
+/** eslint-disable no-underscore-dangle */
+const __filename = fileURLToPath( import.meta.url );
+const __dirname = path.dirname( __filename );
+const compat = new FlatCompat( {
 	baseDirectory: __dirname,
 	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all
-});
+	allConfig: js.configs.all,
+} );
+/** eslint-enable no-underscore-dangle */
 
 export default [
-	...compat.extends("wikimedia"),
+	...compat.extends( 'wikimedia' ),
 	{
-		files: [ "**/*.vue", "**/*.ts", "**/*.js" ],
+		files: [ '**/*.vue', '**/*.ts', '**/*.js', '*.mjs' ],
 		plugins: {
-			"@typescript-eslint": typescriptEslint,
+			'@typescript-eslint': typescriptEslint,
 		},
 
 		languageOptions: {
@@ -31,10 +33,10 @@ export default [
 
 			parser: parser,
 			ecmaVersion: 7,
-			sourceType: "module",
+			sourceType: 'module',
 
 			parserOptions: {
-				parser: "@typescript-eslint/parser",
+				parser: '@typescript-eslint/parser',
 
 				ecmaFeatures: {
 					modules: true,
@@ -49,48 +51,48 @@ export default [
 			// "@typescript-eslint/no-wrapper-object-types": "off",
 			// "@typescript-eslint/ban-ts-comment": "off",
 
-			"no-console": "error",
-			"no-debugger": "error",
+			'no-console': 'error',
+			'no-debugger': 'error',
 
 			// problematic in TypeScript / ES6
-			"no-unused-vars": "off",
-			"@typescript-eslint/no-unused-vars": ["error", {
-				args: "after-used",
-				argsIgnorePattern: "^_",
-				caughtErrors: "none",
-				caughtErrorsIgnorePattern: "^_",
-				destructuredArrayIgnorePattern: "^_",
-				varsIgnorePattern: "^_",
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [ 'error', {
+				args: 'after-used',
+				argsIgnorePattern: '^_',
+				caughtErrors: 'none',
+				caughtErrorsIgnorePattern: '^_',
+				destructuredArrayIgnorePattern: '^_',
+				varsIgnorePattern: '^_',
 				ignoreRestSiblings: true,
-			}],
+			} ],
 
-			"one-var": "off",
-			"no-undef": "off",
+			'one-var': 'off',
+			'no-undef': 'off',
 
 			// diverging from Wikimedia rule set
 			// We can remove these exceptions and settings when we have our own coding style (based on the Wikimedia one)
-			"max-len": ["error", 170],
-			"comma-dangle": ["error", "always-multiline"],
-			"operator-linebreak": "off",
-			"quote-props": "off",
+			'max-len': [ 'error', 170 ],
+			'comma-dangle': [ 'error', 'always-multiline' ],
+			'operator-linebreak': 'off',
+			'quote-props': 'off',
 
 			// Typescript helps with documentation, don't require params and retuns
-			"jsdoc/require-param": "off",
-			"jsdoc/require-returns": "off",
-			"jsdoc/require-param-type": "off",
+			'jsdoc/require-param': 'off',
+			'jsdoc/require-returns': 'off',
+			'jsdoc/require-param-type': 'off',
 
 			// Allow template literals
-			quotes: ["error", "single", {
+			quotes: [ 'error', 'single', {
 				allowTemplateLiterals: true,
-			}],
+			} ],
 
 		},
 	},
 	{
-		files: ["**/*.vue"],
+		files: [ '**/*.vue' ],
 		plugins: {
 			vue: pluginVue,
-			"vuejs-accessibility": vuejsAccessibility,
+			'vuejs-accessibility': vuejsAccessibility,
 
 		},
 		languageOptions: {
@@ -105,19 +107,19 @@ export default [
 			...pluginVue.configs.recommended.base,
 			...vuejsAccessibility.configs.recommended.rules,
 			// Turn max len off to allow for long SVG components
-			"max-len": "off",
+			'max-len': 'off',
 
-			"vue/no-unused-components": ["error", {
+			'vue/no-unused-components': [ 'error', {
 				ignoreWhenBindingPresent: false,
 			} ],
-			"vuejs-accessibility/label-has-for": ["error", {
+			'vuejs-accessibility/label-has-for': [ 'error', {
 				required: {
-					some: ["nesting", "id"],
+					some: [ 'nesting', 'id' ],
 				},
-			}],
+			} ],
 
 			// We use autofocus to ensure the first form field in dialog modals is focused on open
-			"vuejs-accessibility/no-autofocus": "off",
-		}
-	}
+			'vuejs-accessibility/no-autofocus': 'off',
+		},
+	},
 ];
