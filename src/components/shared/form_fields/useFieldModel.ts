7f7775dacdd669b3765b8ee21eece7817083c026
Abban Dunne <Abban@users.noreply.github.com>
2025-03-27 15:00:48 +0100

    Update dependencies (#603)
    Also adjusts the return type of useFieldModel to align
    with changes in Vuejs

--- src/components/shared/form_fields/useFieldModel.ts
+++ src/components/shared/form_fields/useFieldModel.ts
@@ -1,6 +1,8 @@
 import { Ref, ref, UnwrapRef, watch } from 'vue';
 
-export function useFieldModel<T>( modelValue: () => UnwrapRef<T>, initialValue: T ): Ref<UnwrapRef<T>> {
+type VueRefReturnType<T> = Ref<any, any> | Ref<T, T> | Ref<UnwrapRef<T>, T | UnwrapRef<T>>;
+
+export function useFieldModel<T>( modelValue: () => UnwrapRef<T>, initialValue: T ): VueRefReturnType<T> {
 	const fieldModel = ref<T>( initialValue );
 
 	watch( modelValue, ( newValue: UnwrapRef<T> ) => {
