287a60c80a3d15d869ff7afc37add7687571160b
Abban Dunne <abban.dunne@wikimedia.de>
2025-04-10 11:31:32 +0200

    Add an icon to target blank links
    Links that open new tabs should be identifiable to donors:
    https://www.w3.org/WAI/WCAG22/Techniques/general/G201
    
    This adds a small icon and some screen reader
    text to do just that.
    
    Ticket: https://phabricator.wikimedia.org/T391429

---
+++ src/components/shared/icons/ExternalLink.vue
@@ -0,0 +1,8 @@
+<template>
+	<span class="is-sr-only">({{ $t( 'opens_in_new_tab' ) }})</span>
+	<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+		<path d="M4 2.5H0.5V9.5H7.5V6" stroke="currentcolor"></path>
+		<path d="M3.5 6.5L9 1" stroke="currentcolor"></path>
+		<path d="M5 0.5H9.5V5" stroke="currentcolor"></path>
+	</svg>
+</template>
