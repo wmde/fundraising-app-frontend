898a26f95a35759bd35a0d09b7aeadec9868e181
Abban Dunne <abban.dunne@wikimedia.de>
2025-04-10 11:31:32 +0200

    Add an icon to target blank links
    Links that open new tabs should be identifiable to donors:
    https://www.w3.org/WAI/WCAG22/Techniques/general/G201
    
    For accessibility purposes this is added as a small svg icon
    with some screen reader only text to inform to all donors
    that our external links open in a new window.
    
    Ticket: https://phabricator.wikimedia.org/T391429

--- src/components/pages/donation_confirmation/DonationSurvey.vue
+++ src/components/pages/donation_confirmation/DonationSurvey.vue
@@ -2,12 +2,13 @@
 	<div class="donation-confirmation-card donation-survey">
 		<h2 class="icon-title"><donor-icon/> {{ $t( 'donation_confirmation_survey_title' ) }}</h2>
 		<p>{{ $t( 'donation_confirmation_survey_content' ) }}</p>
-		<p><a target="_blank" :href="$t( 'donation_confirmation_survey_link', { tracking: tracking } )">{{ $t( 'donation_confirmation_survey_link_text' ) }}</a></p>
+		<p><a target="_blank" :href="$t( 'donation_confirmation_survey_link', { tracking: tracking } )">{{ $t( 'donation_confirmation_survey_link_text' ) }} <ExternalLink/></a></p>
 	</div>
 </template>
 
 <script setup lang="ts">
 import DonorIcon from '@src/components/shared/icons/DonorIcon.vue';
+import ExternalLink from '@src/components/shared/icons/ExternalLink.vue';
 
 interface Props {
 	tracking: string;
