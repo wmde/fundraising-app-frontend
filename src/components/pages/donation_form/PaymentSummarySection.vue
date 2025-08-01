1a2c0fb1379c1b278e32ada79b653226861240ba
Sperling-0 <tanuja.doriya@wikimedia.de>
2025-08-01 10:24:11 +0200

    Apply Pattern Library to the Donation Summary
    - Add new modular components wherever needed
    
    Ticket: https://phabricator.wikimedia.org/T399537

---
+++ src/components/pages/donation_form/PaymentSummarySection.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h3>{{ $t('donation_form_summary_bank_details') }}</h3>
+		<strong>{{ $t('donation_form_summary_iban') }}</strong> {{ bankData.iban }}<br>
+		<strong v-if="bankData.bic">{{ $t('donation_form_summary_bic') }}</strong> {{ bankData.bic }}<br>
+		<strong v-if="bankData.bankName">{{ $t('donation_form_summary_bank_name') }}</strong> {{ bankData.bankName }}
+	</div>
+</template>
+
+<script setup lang="ts">
+
+interface Props {
+	bankData: { iban: string; bic: string; bankName: string } | undefined;
+}
+defineProps<Props>();
+</script>
---
+++ src/components/patterns/Summary.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="summary switcher">
+		<div class="flow">
+			<slot  name="left"/>
+		</div>
+		<div class="flow">
+			<slot name="right"/>
+		</div>
+	</div>
+</template>
