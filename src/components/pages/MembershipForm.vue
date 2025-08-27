19a65e27d7970af446fd3efc9d2260fae4e6ce10
Sperling-0 <tanuja.doriya@wikimedia.de>
2025-08-27 12:23:47 +0200

    Membership summary headline using pattern library
    Ticket: https://phabricator.wikimedia.org/T399536

--- src/components/pages/MembershipForm.vue
+++ src/components/pages/MembershipForm.vue
@@ -127,7 +127,8 @@
 			<ContentCard>
 
 				<template #heading>
-					<h3>{{ $t( 'membership_confirmation_thanks_text' ) }}</h3>
+					<h2>{{ $t( 'membership_form_summary_title' ) }}</h2>
+					<MembershipSummaryHeadline :payment="membershipApplication"/>
 				</template>
 
 				<template #content>
@@ -197,6 +198,7 @@ import type { MembershipApplication } from '@src/Domain/Membership/MembershipApp
 import type { MembershipAddress } from '@src/Domain/Membership/MembershipAddress';
 import SubmitValues from '@src/components/pages/membership_form/SubmitValues.vue';
 import ScrollTarget from '@src/components/shared/ScrollTarget.vue';
+import MembershipSummaryHeadline from '@src/components/pages/membership_form/MembershipSummaryHeadline.vue';
 
 interface Props {
 	validateAddressUrl: string;
