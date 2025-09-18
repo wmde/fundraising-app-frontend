b550208d1c74c6d2d97f5e1eaf6818f24cb61d68
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2025-09-18 17:49:20 +0200

    Add tests

---
+++ tests/unit/components/pages/MembershipFeeUpgrade.spec.ts
@@ -0,0 +1,25 @@
+import { mount } from '@vue/test-utils';
+
+describe( 'MembershipFeeUpgrade.vue', () => {
+	const getWrapper = () => {};
+
+	test( 'suggestedAmountIsPreselected', () => {
+	} );
+	test( 'selectingCustomAmountsDeselectsSuggestedAmount', () => {
+	} );
+	test( 'reselectingCustomAmountDeselectsSuggestedAmount', () => {
+	} );
+	test( 'customAmountGetsCleared', () => {
+	} );
+	test( 'externalMemberIDIsVisibleInSidebar', () => {
+	} );
+	test( 'showsMemberNameErrorMessage', () => {
+	} );
+	test( 'HidesMemberNameErrorMessage', () => {
+	} );
+	test( 'showsIbanErrorMessage', () => {
+	} );
+	test( 'HidesIbanErrorMessage', () => {
+	} );
+
+} );
