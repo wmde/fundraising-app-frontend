68cad208a04d692bdc4b121b5f1e3211cae99e54
Abban Dunne <abban.dunne@wikimedia.de>
2025-09-19 13:01:21 +0200

    Set up membership fee upgrade tests
    This adds the basic setup to begin testing the
    MembershipFeeUpgrade component.
    
    Ticket: https://phabricator.wikimedia.org/T396008

--- tests/unit/components/pages/MembershipFeeUpgrade.spec.ts
+++ tests/unit/components/pages/MembershipFeeUpgrade.spec.ts
@@ -1,9 +1,36 @@
-import { mount } from '@vue/test-utils';
+import { mount, VueWrapper } from '@vue/test-utils';
+import MembershipFeeUpgrade from '@src/components/pages/MembershipFeeUpgrade.vue';
+import { newSucceedingBankValidationResource } from '@test/unit/TestDoubles/SucceedingBankValidationResource';
+
+const uuid = '28178402-56ef-4977-87d9-c681c29bb823';
+const currentAmountInCents = 1000;
+const suggestedAmountInCents = 1600;
+const currentInterval = 3;
+const feeChangeFrontendFlag = 'SHOW_FEE_CHANGE_FORM';
 
 describe( 'MembershipFeeUpgrade.vue', () => {
-	const getWrapper = () => {};
+	const getWrapper = (): VueWrapper<any> => {
+		return mount( MembershipFeeUpgrade, {
+			props: {
+				uuid,
+				currentAmountInCents,
+				suggestedAmountInCents,
+				currentInterval,
+				feeChangeFrontendFlag,
+			},
+			global: {
+				provide: {
+					bankValidationResource: newSucceedingBankValidationResource(),
+					membershipFeeChangeResource: { put: () => Promise.resolve( { status: 'OK' } ) },
+				},
+			},
+		} );
+	};
 
 	test( 'suggestedAmountIsPreselected', () => {
+		const wrapper = getWrapper();
+
+		expect( wrapper.find<HTMLInputElement>( '#suggested-amount' ).element.checked ).toBeTruthy();
 	} );
 	test( 'selectingCustomAmountsDeselectsSuggestedAmount', () => {
 	} );
