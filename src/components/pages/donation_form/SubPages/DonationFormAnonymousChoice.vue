8872d0d03feb3e475cc782ea39fbac33761e1a6b
Sperling-0 <30310702+Sperling-0@users.noreply.github.com>
2025-09-23 18:23:50 +0200

    Use shared component for form summary (#735)
    - Delete unused component

--- src/components/pages/donation_form/SubPages/DonationFormAnonymousChoice.vue
+++ src/components/pages/donation_form/SubPages/DonationFormAnonymousChoice.vue
@@ -40,7 +40,7 @@
 		<template #content>
 			<Summary v-if="addressSummary || bankDataSummary">
 				<template #left v-if="addressSummary">
-					<DonorSummarySection
+					<AddressSummarySection
 						:address="addressSummary"
 						:countries="countries"
 						:salutations="salutations"
@@ -102,7 +102,7 @@ import { useAddressOptOutModel } from '@src/components/pages/donation_form/Addre
 import { useBankDataSummary } from '@src/components/pages/donation_form/useBankDataSummary';
 import PaymentSummarySection from '@src/components/shared/PaymentSummarySection.vue';
 import ContentCard from '@src/components/patterns/ContentCard.vue';
-import DonorSummarySection from '@src/components/pages/donation_form/DonorSummarySection.vue';
+import AddressSummarySection from '@src/components/shared/AddressSummarySection.vue';
 import DonationSummaryHeadline from '@src/components/pages/donation_form/DonationSummaryHeadline.vue';
 import Summary from '@src/components/patterns/Summary.vue';
 
