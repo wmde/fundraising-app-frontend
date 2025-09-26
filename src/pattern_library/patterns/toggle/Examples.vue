5cf2396990ff79e7a69f859fd3c46894be81f07d
Abban Dunne <abban.dunne@wikimedia.de>
2025-09-26 08:43:17 +0200

    Add toggle pattern
    Adds styles that make a checkbox look like a toggle
    
    Ticket: https://phabricator.wikimedia.org/T405435

---
+++ src/pattern_library/patterns/toggle/Examples.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="content-card flow">
+		<div class="section-heading">
+			<h2>Toggle</h2>
+			<hr/>
+		</div>
+
+		<div class="field-container flow">
+			<label>
+				Spendenquittung
+				<input type="checkbox" name="toggle" class="toggle" id="toggle">
+			</label>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+defineOptions( { inheritAttrs: false } );
+</script>
