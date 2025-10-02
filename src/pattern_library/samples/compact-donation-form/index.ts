d03fc0eea41b6c91bcd53a6de219fe2b0b11bf78
Abban Dunne <abban.dunne@wikimedia.de>
2025-10-02 13:04:26 +0200

    Add flex field group composition
    This is a new composition that allows us to
    layout groups of fields in different ways.
    
    Adds a sample of the compact form to the pattern
    library.
    
    Adds a variant to the field container for explicitly
    specifying a max width field. Individual fields
    should fill their containers, and we should use
    compositions to control the layout.
    
    Ticket: https://phabricator.wikimedia.org/T405438

---
+++ src/pattern_library/samples/compact-donation-form/index.ts
@@ -0,0 +1,7 @@
+import { Sample } from '@src/pattern_library/samples/Sample';
+
+const name = 'Compact Donation Form';
+const url = 'compact-donation-form';
+import component from './Sample.vue';
+
+export default { name, url, component } as Sample;
