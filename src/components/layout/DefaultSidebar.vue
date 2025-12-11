a077209979c6e777a245353cb8b6f72b117d8dea
Abban Dunne <abban.dunne@wikimedia.de>
2025-12-11 09:01:38 +0100

    Add membership sidebar card
    - Move default sidebar cards into shared components
    - Update member fee change sidebar test and remove
      unneeded IDs and classses
    - Add new member form sidebar
    
    Ticket: https://phabricator.wikimedia.org/T412196

---
+++ src/components/layout/DefaultSidebar.vue
@@ -0,0 +1,9 @@
+<template>
+	<GetInTouch/>
+	<BankInfo/>
+</template>
+
+<script setup lang="ts">
+import GetInTouch from '@src/components/shared/sidebar_cards/GetInTouch.vue';
+import BankInfo from '@src/components/shared/sidebar_cards/BankInfo.vue';
+</script>
---
+++ src/components/shared/sidebar_cards/BankInfo.vue
@@ -0,0 +1,23 @@
+<template>
+	<ContentCard :is-sidebar-card="true">
+		<template #content>
+			<IconText :is-small-heading="true">
+				<template #icon><BankIcon class="highlighted-icon"/></template>
+				<template #content>
+					<h2>
+						<span class="highlighted-content-text">{{ $t('bank_data_title_new_prefix') }}</span>
+						{{ $t('bank_data_title') }}
+					</h2>
+				</template>
+			</IconText>
+			<BankData/>
+		</template>
+	</ContentCard>
+</template>
+<script setup lang="ts">
+
+import BankData from '@src/components/shared/BankData.vue';
+import IconText from '@src/components/patterns/IconText.vue';
+import ContentCard from '@src/components/patterns/ContentCard.vue';
+import BankIcon from '@src/components/shared/icons/BankIcon.vue';
+</script>
