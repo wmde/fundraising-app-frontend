3b74d6cae0909c33254e555b4b885d580e5769b8
Sperling-0 <tanuja.doriya@wikimedia.de>
2026-09-22 08:57:46 +0200

    Add the payment action
    - It's being called by an action in the address actions, adding the payment actions to the new store so it can be found.
    - This will resolve following error: `[vuex] unknown action type: membership_fee/resetFeeForAddressType`
    
    Ticket: https://phabricator.wikimedia.org/T427606

--- src/store/membership_applicant_update_store.ts
+++ src/store/membership_applicant_update_store.ts
@@ -1,11 +1,13 @@
 import Vuex, { StoreOptions } from 'vuex';
 import createAddress from '@src/store/membership_address';
+import createPayment from '@src/store/membership_fee';
 import { DEFAULT_FIELDS, REQUIRED_FIELDS_MEMBERSHIP_APPLICANT_UPDATE } from '@src/store/membership_address/constants';
 
 export function createStore() {
 	const storeBundle: StoreOptions<any> = {
 		modules: {
 			[ 'address' ]: createAddress( REQUIRED_FIELDS_MEMBERSHIP_APPLICANT_UPDATE, DEFAULT_FIELDS ),
+			[ 'membership_fee' ]: createPayment(),
 		},
 		strict: process.env.NODE_ENV !== 'production',
 		getters: {
