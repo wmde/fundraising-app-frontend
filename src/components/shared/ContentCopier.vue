645fdd830711dd1a9214a1e86de9f4be4ef8e008
Lucas Werkmeister <lucas.werkmeister@wikimedia.de>
2026-09-24 14:25:33 +0200

    ContentCopier: Use display-togger utility
    Note that the display-toggler utility uses `display: none` instead of
    `visibility: hidden`, so it’s possible that this changes the
    accessibility behavior for the worse (the aria-live="assertive" element
    now toggles its display instead of its visibility + contents); I haven’t
    tested it.
    
    Ticket: https://phabricator.wikimedia.org/T438318

--- src/components/shared/ContentCopier.vue
+++ src/components/shared/ContentCopier.vue
@@ -1,6 +1,6 @@
 <template>
   <button class="content-copier" :class="{ 'content-copier--copied': wasJustCopied }" @click="onCopy" @touchstart="onCopy">
-    <span class="content-copier__popup" aria-live="assertive"><template v-if="wasJustCopied">{{ $t( 'copy_popup' ) }}</template></span>
+    <span class="content-copier__popup display-toggler" :class="{ 'display-toggler--visible': wasJustCopied }" aria-live="assertive"><template v-if="wasJustCopied">{{ $t( 'copy_popup' ) }}</template></span>
     <strong v-if="label">{{ label }}:</strong> {{ value }}
     <span class="visually-hidden">{{ $t( 'copy_click_message' ) }}</span>
     <CopyIcon/>
@@ -61,17 +61,10 @@ const onCopy = (): void => {
 .content-copier__popup {
   position: absolute;
   inset: 0;
-  visibility: hidden;
-  opacity: 0;
   background: var( --content-copier-copied-background, #dadada );
   color: var( --content-copier-copied-color, #000000 );
   text-align: center;
   border-radius: 3px;
-}
-
-.content-copier--copied > .content-copier__popup {
-  visibility: visible;
-  opacity: 1;
 }
 
 .content-copier > svg {
